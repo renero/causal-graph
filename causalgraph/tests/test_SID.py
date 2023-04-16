e7e418054296279e681f642ce4c599cd77da5958
jesus.renero <jesus.renero@gmail.com>
2023-04-16 13:41:56 +0200

    all tests

--- causalgraph/tests/test_SID.py
+++ causalgraph/tests/test_SID.py
@@ -3,30 +3,48 @@ import pytest
 
 from causalgraph.metrics.SID import SID
 
+
 def test_SID():
     G = np.array([[0, 1, 1, 1, 1],
                   [0, 0, 1, 1, 1],
                   [0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0]])
-
     H1 = np.array([[0, 1, 1, 1, 1],
                    [0, 0, 1, 1, 1],
                    [0, 0, 0, 1, 0],
                    [0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0]])
-
     H2 = np.array([[0, 0, 1, 1, 1],
                    [1, 0, 1, 1, 1],
                    [0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0]])
-
-    H1c = np.array([[0, 1, 1, 1, 1],
-                    [1, 0, 1, 1, 1],
-                    [1, 1, 0, 1, 0],
-                    [1, 1, 1, 0, 0],
-                    [1, 1, 0, 0, 0]])
+    H3 = np.array([[0, 1, 1, 1, 1],
+                   [1, 0, 1, 1, 1],
+                   [1, 1, 0, 1, 0],
+                   [1, 1, 1, 0, 0],
+                   [1, 1, 0, 0, 0]])
+    H4 = np.array([[0, 0, 1, 1, 1],
+                   [0, 0, 0, 1, 0],
+                   [1, 1, 0, 1, 0],
+                   [0, 1, 0, 0, 1],
+                   [1, 0, 1, 0, 0]])
+    H5 = np.array([[0, 0, 1, 1, 0],
+                   [0, 0, 0, 0, 1],
+                   [0, 0, 0, 1, 0],
+                   [0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 0]])
 
     sid1 = SID(G, H1)
     assert sid1['sid'] == 0.0
+    sid2 = SID(G, H2)
+    assert sid2['sid'] == 8.0
+    sid3 = SID(G, H3)
+    assert sid3['sidLowerBound'] == 0.0
+    assert sid3['sidUpperBound'] == 15.0
+    sid4 = SID(G, H4)
+    assert sid4['sidLowerBound'] == 8.0
+    assert sid4['sidUpperBound'] == 16.0
+    sid5 = SID(G, H5)
+    assert sid5['sid'] == 12.0
