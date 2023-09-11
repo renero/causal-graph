25112d48e0df1811d8849953c6dcf4cc3373d638
Jesus Renero Quintero <jesus.renero@gmail.com>
2023-09-11 16:11:11 +0200

    outliers based modified z-scores

--- causalgraph/explainability/regression_quality.py
+++ causalgraph/explainability/regression_quality.py
@@ -5,85 +5,62 @@ from sklearn.base import BaseEstimator
 
 
 class RegQuality(BaseEstimator):
-    
+
     def __init__(self):
         super().__init__()
-        
+
     @staticmethod
     def predict(
-        scores: List[float],
-        gamma_shape: float=1,
-        gamma_scale: float=1,
-        threshold: float=0.9,
-        # iqr_multiplier: float=2.5,
-        verbose: bool=False) -> Set[int]:
+            scores: List[float],
+            gamma_shape: float = 1,
+            gamma_scale: float = 1,
+            threshold: float = 0.9,
+            verbose: bool = False) -> Set[int]:
         """
         Returns the indices of features that are both gamma and outliers. Both criteria
         are applied to the given scores to determine if the MSE error obtained from
         the regression is bad compared with the rest of regressions for the other features
         in the dataset, and thus the feature should be considered a parent node.
-    
+
         Parameters
         ----------
         scores: List[float]
             List of scores
-        
+
         Returns
         -------
         Set[int]
             List of indices of features that are both gamma and outliers
         """
         scores = np.array(scores)
-        gamma_indices = [idx for idx, score in enumerate(scores) \
-            if stats.gamma.pdf(score, a=gamma_shape, scale=gamma_scale) <= threshold]
-    
-        # median = np.median(scores)
-        # iqr = stats.iqr(scores)
-        # outliers_indices = [idx for idx, score in enumerate(scores) \
-        #     if np.abs(score-median) > iqr_multiplier*iqr]
+        gamma_indices = RegQuality._gamma_criteria(
+            scores, gamma_shape, gamma_scale, threshold, verbose=verbose)
         outliers_indices = RegQuality._mad_criteria(scores, verbose=verbose)
-    
+
         return set(gamma_indices).intersection(outliers_indices)
-    
+
     @staticmethod
-    def _iqr_criteria(scores, multiplier:float=2.5, verbose=False) -> Set[int]:
+    def _mad_criteria(scores, verbose=False) -> Set[int]:
         """
-        Returns indices of outliers in the given scores, using the 2.5*IQR method.
+        Returns indices of outliers in the given scores, using the MAD method.
+        Taken from https://stats.stackexchange.com/a/78617
         
         Parameters
         ----------
         scores: List[float]
             List of scores
-        multiplier: float
-            Multiplier for the IQR
         verbose: bool
-            Whether to print the score and IQR criteria for each score
+            Whether to print the score and M for each score
             
         Returns
         -------
         Set[int]
-            List of indices of outliers
-        """
-        iqr = np.quantile(scores, 0.75) - np.quantile(scores, 0.25)
-        median = np.median(scores)
-        outliers_indices = np.where(np.abs(scores-median) > multiplier*iqr)
-        if verbose:
-            print(f"Scorings (MSE): {scores}")
-            print(f"{multiplier:.1f}·IQR: {multiplier*iqr:.4f}"
-                f" - Median.: {median:.4f}")
-            print(f"Outliers indices: {outliers_indices}")
-            
-        return set(outliers_indices[0])
-    
-    @staticmethod
-    def _mad_criteria(scores, verbose=False) -> Set[int]:
-        """
-        Returns indices of outliers in the given scores, using the MAD method.
+            List of indices of outliers in the given scores
         """
         median = np.median(scores)
         ad = [np.abs(score-median) for score in scores]
         mad = np.median(ad)
-        M = [ np.abs(.6745 * (score-median) / mad) for score in scores ]
+        M = [np.abs(.6745 * (score-median) / mad) for score in scores]
         mad_indices = [idx for idx, m in enumerate(M) if m > 3.5]
 
         if verbose:
@@ -91,20 +68,19 @@ class RegQuality(BaseEstimator):
             print(f"Median of absolute differences: {mad:.4f}")
             for score, m in zip(scores, M):
                 print(f"Score: {score:.4f}, M: {m:.4f}")
-            
+
         return set(mad_indices)
 
-    
     @staticmethod
     def _gamma_criteria(
-        scores, 
-        gamma_shape=1, 
-        gamma_scale=1, 
-        threshold=0.9, 
-        verbose=False) -> Union[None, Set[int]]:
+            scores,
+            gamma_shape=1,
+            gamma_scale=1,
+            threshold=0.9,
+            verbose=False) -> Union[None, Set[int]]:
         """
         Returns a list of booleans indicating whether the score is below the threshold
-        
+
         Parameters
         ----------
         scores: List[float]
@@ -117,7 +93,7 @@ class RegQuality(BaseEstimator):
             Threshold for gamma criteria
         verbose: bool
             Whether to print the score and gamma criteria for each score
-            
+
         Returns
         -------
         Set[int]
