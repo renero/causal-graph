d1aa9e8de3f2550adf92479d988f0412ec2e005d
Jesus Renero Quintero <jesus.renero@gmail.com>
2024-12-09 12:23:20 +0100

    Another attempt to link modules doc from main modules section

--- causalgraph/models/__init__.py
+++ causalgraph/models/__init__.py
@@ -1,2 +1,22 @@
-from causalgraph.models.dnn import NNRegressor
-from causalgraph.models.gbt import GBTRegressor
+"""
+CausalGraph Models
+==================
+
+This module contains the models for causal discovery methods. All the models
+are implemented in the scikit-learn style, with a `fit` method to fit the model
+to the data, a `predict` method to make predictions, and a `score` method to
+evaluate the model performance.
+
+The models are:
+
+- `GBTRegressor`: Gradient Boosting Trees Regressor
+- `NNRegressor`: Neural Network Regressor
+"""
+
+from .gbt import GBTRegressor
+from .dnn import NNRegressor
+
+__all__ = [
+    'GBTRegressor',
+    'NNRegressor',
+]
